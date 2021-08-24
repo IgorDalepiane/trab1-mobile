3143d5946cfc735938dd4378476e44449562033e
Lucas Alexandre Fell <47724710+fell-lucas@users.noreply.github.com>
2021-08-24 00:12:24 -0300

    padding

--- lib/screens/about_screen.dart
+++ lib/screens/about_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:trab1_mobile/widgets/bottom_nav_bar.dart';
+import 'package:trab1_mobile/widgets/custom_appbar.dart';
 
 class AboutScreen extends StatelessWidget {
   const AboutScreen({Key? key}) : super(key: key);
@@ -7,30 +8,11 @@ class AboutScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Color(0xffffffca),
-        toolbarHeight: 160,
-        bottom: PreferredSize(
-          child: Container(
-            color: Colors.black,
-            height: 2.0,
-          ),
-          preferredSize: Size.fromHeight(4.0),
-        ),
-        title: Text(
-          'Receitas Delit',
-          style: TextStyle(
-            color: Colors.black,
-            fontFamily: "Righteous",
-            fontSize: 40,
-            fontWeight: FontWeight.w700,
-          ),
-        ),
-      ),
+      appBar: CustomAppBar(),
       bottomNavigationBar: BottomNavBar(selectedIndex: 2),
       body: Container(
         child: SingleChildScrollView(
-          padding: EdgeInsets.fromLTRB(20, 30, 20, 0),
+          padding: EdgeInsets.fromLTRB(20, 30, 20, 30),
           child: Column(
             mainAxisAlignment: MainAxisAlignment.spaceBetween,
             crossAxisAlignment: CrossAxisAlignment.start,
