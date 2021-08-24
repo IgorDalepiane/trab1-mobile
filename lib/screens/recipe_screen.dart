7de55a12e080daf02be37fcdd2111509929f7f1e
Lucas Alexandre Fell <47724710+fell-lucas@users.noreply.github.com>
2021-08-24 00:09:22 -0300

    recipe screen

--- lib/screens/recipe_screen.dart
+++ lib/screens/recipe_screen.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:trab1_mobile/widgets/bottom_nav_bar.dart';
 import 'package:trab1_mobile/widgets/custom_appbar.dart';
+import 'package:trab1_mobile/widgets/custom_card_nodesc.dart';
 
 class RecipeScreen extends StatelessWidget {
   const RecipeScreen({Key? key}) : super(key: key);
@@ -8,6 +10,55 @@ class RecipeScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: CustomAppBar(),
+      bottomNavigationBar: BottomNavBar(selectedIndex: 1),
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            CustomCardNoDesc(
+              logoPath: 'assets/images/cacetinho.png',
+              title: 'Cacetinho no Balaio',
+              subTitle: 'Delícia tradicional',
+              recipePage: 'cacetinho',
+            ),
+            CustomCardNoDesc(
+              logoPath: 'assets/images/cacetinho.png',
+              title: 'Cacetinho no Balaio',
+              subTitle: 'Delícia tradicional',
+              recipePage: 'cacetinho',
+            ),
+            CustomCardNoDesc(
+              logoPath: 'assets/images/cacetinho.png',
+              title: 'Cacetinho no Balaio',
+              subTitle: 'Delícia tradicional',
+              recipePage: 'cacetinho',
+            ),
+            CustomCardNoDesc(
+              logoPath: 'assets/images/cacetinho.png',
+              title: 'Cacetinho no Balaio',
+              subTitle: 'Delícia tradicional',
+              recipePage: 'cacetinho',
+            ),
+            CustomCardNoDesc(
+              logoPath: 'assets/images/cacetinho.png',
+              title: 'Cacetinho no Balaio',
+              subTitle: 'Delícia tradicional',
+              recipePage: 'cacetinho',
+            ),
+            CustomCardNoDesc(
+              logoPath: 'assets/images/cacetinho.png',
+              title: 'Cacetinho no Balaio',
+              subTitle: 'Delícia tradicional',
+              recipePage: 'cacetinho',
+            ),
+            CustomCardNoDesc(
+              logoPath: 'assets/images/cacetinho.png',
+              title: 'Cacetinho no Balaio',
+              subTitle: 'Delícia tradicional',
+              recipePage: 'cacetinho',
+            )
+          ],
+        ),
+      ),
     );
   }
 }
