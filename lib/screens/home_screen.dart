ba44b29958392fc1cddbb3f099afb083feec6ced
Lucas Alexandre Fell <47724710+fell-lucas@users.noreply.github.com>
2021-08-23 23:03:14 -0300

    cards index

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -75,46 +75,132 @@ class _HomeScreenState extends State<HomeScreen> {
         ),
       ),
       backgroundColor: Color(0xfff7f7e3),
-      body: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          Container(
-            padding: EdgeInsets.only(
-              top: 10.0,
-              bottom: 30.0,
-            ),
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                Container(
-                  child: CarouselSlider(
-                    options: CarouselOptions(
-                      autoPlay: true,
-                      aspectRatio: 2.0,
-                      enlargeCenterPage: true,
-                    ),
-                    items: imgList
-                        .map(
-                          (item) => Container(
-                            child: Center(
-                              child: ClipRRect(
-                                borderRadius: BorderRadius.circular(8.0),
-                                child: Image.asset(
-                                  item,
-                                  fit: BoxFit.cover,
-                                  width: 1000,
+      body: SingleChildScrollView(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Container(
+              padding: EdgeInsets.only(
+                top: 10.0,
+                bottom: 30.0,
+              ),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Container(
+                    child: CarouselSlider(
+                      options: CarouselOptions(
+                        autoPlay: true,
+                        aspectRatio: 2.0,
+                        enlargeCenterPage: true,
+                      ),
+                      items: imgList
+                          .map(
+                            (item) => Container(
+                              child: Center(
+                                child: ClipRRect(
+                                  borderRadius: BorderRadius.circular(8.0),
+                                  child: Image.asset(
+                                    item,
+                                    fit: BoxFit.cover,
+                                    width: 1000,
+                                  ),
                                 ),
                               ),
                             ),
-                          ),
-                        )
-                        .toList(),
+                          )
+                          .toList(),
+                    ),
                   ),
-                ),
-              ],
+                  Column(
+                    children: [
+                      Padding(
+                        padding: const EdgeInsets.symmetric(
+                            vertical: 6, horizontal: 12),
+                        child: Card(
+                          clipBehavior: Clip.antiAlias,
+                          child: Column(
+                            children: [
+                              ListTile(
+                                leading:
+                                    Image.asset('assets/images/cacetinho.png'),
+                                title: const Text('Cacetinho no Balaio'),
+                                subtitle: Text(
+                                  'Delícia tradicional',
+                                  style: TextStyle(
+                                      color: Colors.black.withOpacity(0.6)),
+                                ),
+                              ),
+                              Padding(
+                                padding: const EdgeInsets.all(16.0),
+                                child: Text(
+                                  'No Rio de Janeiro, conta-se que o primeiro gaúcho a pedir um Cacetinho foi enrabado por 5 padeiros ao mesmo tempo. Desde então, todo gaúcho, ao viajar para o Rio, ganha um folheto explicativo sobre como chamar pão francês fora de sua terra colorida.',
+                                  style: TextStyle(
+                                      color: Colors.black.withOpacity(0.6)),
+                                ),
+                              ),
+                              ButtonBar(
+                                alignment: MainAxisAlignment.start,
+                                children: [
+                                  TextButton(
+                                    onPressed: () {
+                                      // Perform some action
+                                    },
+                                    child: const Text('VER RECEITA'),
+                                  ),
+                                ],
+                              ),
+                            ],
+                          ),
+                        ),
+                      ),
+                      Padding(
+                        padding: const EdgeInsets.symmetric(
+                            vertical: 6, horizontal: 12),
+                        child: Card(
+                          clipBehavior: Clip.antiAlias,
+                          child: Column(
+                            children: [
+                              ListTile(
+                                leading:
+                                    Image.asset('assets/images/ovofrito.png'),
+                                title: const Text('Pão com Ovo'),
+                                subtitle: Text(
+                                  'Delícia tradicional',
+                                  style: TextStyle(
+                                      color: Colors.black.withOpacity(0.6)),
+                                ),
+                              ),
+                              Padding(
+                                padding: const EdgeInsets.all(16.0),
+                                child: Text(
+                                  'Tão importante quanto o sanduíche de presunto e o sanduíche-íche, o sanduba de ovo foi criado pela Dona Florinda em 1954 e até hoje é o mais vendido sanduíche do mundo. Tendo inclusive o Godinez desenhado a versão mais famosa do sanduíche-íche para o Professor Girafales.',
+                                  style: TextStyle(
+                                      color: Colors.black.withOpacity(0.6)),
+                                ),
+                              ),
+                              ButtonBar(
+                                alignment: MainAxisAlignment.start,
+                                children: [
+                                  TextButton(
+                                    onPressed: () {
+                                      // Perform some action
+                                    },
+                                    child: const Text('VER RECEITA'),
+                                  ),
+                                ],
+                              ),
+                            ],
+                          ),
+                        ),
+                      ),
+                    ],
+                  )
+                ],
+              ),
             ),
-          ),
-        ],
+          ],
+        ),
       ),
     );
   }
